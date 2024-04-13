c6300757e7eb59530bdba08ace630867dc7d49de
Hardikgir <hardik.meghnathi12@gmail.com>
2024-04-13 18:36:09 +0530

    Dev

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CODEIGNITER_PHP/Verifyfa/Dev/';
+$config['base_url'] = 'https://verifyfa.developmentdemo.co.in/';
 
 /*
 |--------------------------------------------------------------------------
