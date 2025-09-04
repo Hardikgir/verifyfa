fd13bc131ee85c020f70b0ae7c5e712f3b49b416
Hardikgiri <hardik.meghnathi12@gmail.com>
2025-09-04 12:46:53 +0530

    Dev

---
+++ Queries1.sql
@@ -0,0 +1,29 @@
+SELECT * FROM v74_ci_verifyfa_live_server_db.users where entity_code = 'PROBUDS';
+SELECT * FROM v74_ci_verifyfa_live_server_db.user_role where entity_code = 'PROBUDS';
+SELECT * FROM v74_ci_verifyfa_live_server_db.company_projects where id in (10,11,12);
+SELECT * FROM v74_ci_verifyfa_live_server_db.03sep_tg1;
+SELECT * FROM v74_ci_verifyfa_live_server_db.03sep_nt;
+SELECT * FROM v74_ci_verifyfa_live_server_db.03sep_tg2;
+
+SELECT ur.*,c.company_name,cl.location_name,CONCAT(u.firstName, ' ', u.lastName) AS fullname,
+       REPLACE(
+         REPLACE(
+           REPLACE(
+             REPLACE(
+               REPLACE(
+                 REPLACE(ur.user_role,
+                   '5', 'Group Admin'),
+               '4', 'Sub Admin'),
+             '0', 'Manager'),
+           '2', 'Process Owner'),
+         '3', 'Entity Owner'),
+       '1', 'Verifier') AS roles
+FROM v74_ci_verifyfa_live_server_db.user_role ur
+LEFT JOIN company c 
+       ON ur.company_id = c.id
+LEFT JOIN company_locations cl 
+       ON ur.location_id = cl.id
+LEFT JOIN users u 
+       ON ur.user_id = u.id       
+where ur.entity_code = 'PROBUDS';
+;
